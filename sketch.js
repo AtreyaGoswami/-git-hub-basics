d49fe1456c970e422e7498b4974f3151738dfc58
AtreyaGoswami <72393490+AtreyaGoswami@users.noreply.github.com>
2020-11-09 18:02:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function preload(){
  monkey_running=loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
   
   obstacleImage=loadImage("stone.png");
-  sceneImage=loadImage("jungle1.jpg");
+  sceneImage=loadImage("jungle 1.jpg");
 }
 
 
